ab5eed442686a77565898fb8bcf55330d3e377a9
darcywilkin <darcywilkin@gmail.com>
2016-02-17 09:19:24 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+
+//$('li').fadeTo('slow', 0.25);
+	//$( this ).hover(function(){
+
+//});//
+
+
+$( "li" ).hover(function() {
+		$(this).stop().animate({"opacity": "0.25"}, "slow");
+	},
+			function() {
+				$(this).stop().animate({"opacity": "1"}, "slow");
+			});		 
+
+
+
+//$(document).ready(function(){
+         /* I'm using the "container" class. You could use any class or ID*/
+//		$(".container img").hover(
+//			function() {
+//				$(this).stop().animate({"opacity": "0.2"}, "slow");
+//			},
+//			function() {
+//				$(this).stop().animate({"opacity": "1"}, "slow");
+//			});		 
+//	});
+
+
+
+
+
+
+
+//var $li = $('li');
+
+
+
+
+//$( "li.fade" ).hover(function() {
+  //$( this ).fadeOut( 100 )  
+   //$( this ).fadeIn( 500 );
+//});
